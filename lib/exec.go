62990c7a8acd59f77dc6c1b21a254899db534ce8
wazi <wazidw@foxmail.com>
2022-08-19 11:36:21 +0800

    add execute timeout kill container && showstderr

--- lib/exec.go
+++ lib/exec.go
@@ -6,6 +6,7 @@ import (
 	"context"
 	"gotribe/compiler/lib/log"
 	"io/ioutil"
+	"time"
 
 	"github.com/docker/docker/api/types"
 	"github.com/docker/docker/api/types/container"
@@ -17,14 +18,21 @@ func DockerRun(image string, code string, dest string, cmd string) string {
 	ctx := context.Background()
 	cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
 	if err != nil {
-		panic(err)
+		log.DefaultLogger.Error("NewClientWithOpts:", err)
 	}
 	resp, err := cli.ContainerCreate(ctx, &container.Config{
-		Image: image,
-		Cmd:   []string{"sh", "-c", cmd},
-	}, nil, nil, nil, "")
+		Image:        image,
+		Cmd:          []string{"sh", "-c", cmd},
+		Tty:          true,
+		AttachStderr: true,
+		AttachStdout: true,
+	}, &container.HostConfig{
+		Resources: container.Resources{
+			Memory: 100 * 1024 * 1024, // Minimum memory limit allowed is 6MB.
+		},
+	}, nil, nil, "")
 	if err != nil {
-		panic(err)
+		log.DefaultLogger.Error("ContainerCreate:", err)
 	}
 
 	var buf bytes.Buffer
@@ -35,35 +43,41 @@ func DockerRun(image string, code string, dest string, cmd string) string {
 		Size: int64(len(code)), // filesize
 	})
 	if err != nil {
-		// panic(err)
 		log.DefaultLogger.Error("docker copy err:", err)
 	}
 	tw.Write([]byte(code))
 	tw.Close()
 
 	// use &buf as argument for content in CopyToContainer
-	cli.CopyToContainer(context.Background(), resp.ID, ".", &buf, types.CopyToContainerOptions{})
+	cli.CopyToContainer(ctx, resp.ID, ".", &buf, types.CopyToContainerOptions{})
 
 	if err := cli.ContainerStart(ctx, resp.ID, types.ContainerStartOptions{}); err != nil {
-		panic(err)
+		log.DefaultLogger.Errorf("ContainerStart err:%v:", err)
 	}
 
 	statusCh, errCh := cli.ContainerWait(ctx, resp.ID, container.WaitConditionNotRunning)
 
+	timeout := time.NewTimer(5 * time.Second)
 	select {
-	case err := <-errCh:
-		if err != nil {
-			panic(err)
-		}
-	case <-statusCh:
+	case waitBody := <-statusCh:
+		log.DefaultLogger.Errorf("waitBody err:%v:", waitBody.StatusCode)
+		break
+	case errC := <-errCh:
+		log.DefaultLogger.Errorf("statusCh err:%v:", errC)
+	case <-timeout.C:
+		log.DefaultLogger.Error("execute timeout")
+		cli.ContainerKill(ctx, resp.ID, "SIGKILL")
+		log.DefaultLogger.Error("ContainerKill")
+		return "execute timeout"
 	}
 
-	out, err := cli.ContainerLogs(ctx, resp.ID, types.ContainerLogsOptions{ShowStdout: true})
+	out, err := cli.ContainerLogs(ctx, resp.ID, types.ContainerLogsOptions{ShowStdout: true, ShowStderr: true})
 	if err != nil {
-		panic(err)
+		log.DefaultLogger.Errorf("ContainerLogs err:%v:", err)
 	}
+
 	defer out.Close()
 	output, _ := ioutil.ReadAll(out)
-	log.DefaultLogger.Errorf("output-------------%q+:", output)
+	log.DefaultLogger.Info("ContainerRemove err:%v:", output)
 	return string(output)
 }
